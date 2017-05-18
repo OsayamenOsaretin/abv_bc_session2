0581770ff56798c0163654a3425ab2c96e2f18cf
yamen <osaretinosayamen@gmail.com>
2017-05-18 05:51:55 +0100

    Reset test cases to original

--- tests/tests.js
+++ tests/tests.js
@@ -1,24 +1,24 @@
 'use strict';
 
 const MyApp = require('../app/library.js')
-
+const fizzBuzz = MyApp.fizzBuzz
 
 describe("Fizz Buzz tests ", function() {
 
   it("should return `Fizz` for number divisible by 3", function() {
-    expect(MyApp.fizzBuzz(3)).toBe('Fizz');
+    expect(fizzBuzz(3)).toBe('Fizz');
   });
 
   it("should return `Buzz` for number divisible by 5", function() {
-    expect(MyApp.fizzBuzz(5)).toBe('Buzz');
+    expect(fizzBuzz(5)).toBe('Buzz');
   });
 
   it("should return `FizzBuzz` for 15", function() {
-    expect(MyApp.fizzBuzz(15)).toBe('FizzBuzz');
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
   });
 
   it("should return `FizzBuzz` for 45", function() {
-    expect(MyApp.fizzBuzz(45)).toBe('FizzBuzz');
+    expect(fizzBuzz(45)).toBe('FizzBuzz');
   });
 
   it("should return `FizzBuzz` for 90", function() {
@@ -26,23 +26,23 @@ describe("Fizz Buzz tests ", function() {
   });
 
   it("should return `Fizz` for 63", function() {
-    expect(MyApp.fizzBuzz(63)).toBe('Fizz');
+    expect(fizzBuzz(63)).toBe('Fizz');
   });
 
   it("should return 7 since its indivisible by 3 and 5", function() {
-    expect(MyApp.fizzBuzz(7)).toBe(7);
+    expect(fizzBuzz(7)).toBe(7);
   });
 
   it("should return 101 since its indivisible by 3 and 5", function() {
-    expect(MyApp.fizzBuzz(101)).toBe(101);
+    expect(fizzBuzz(101)).toBe(101);
   });
 
   it("should return error message if no argument is passed", function(){
-  	expect(MyApp.fizzBuzz()).toBe('Please enter an Integer to the FizzBuzz calculator');
+  	expect(fizzBuzz()).toBe('Please enter an Integer to the FizzBuzz calculator');
   });
 
   it("should return error message if float is passed instead of integer", function(){
-  	expect(MyApp.fizzBuzz(4.5)).toBe('Please enter an Integer, instead of a float');
+  	expect(fizzBuzz(4.5)).toBe('Please enter an Integer, instead of a float');
   });
 
 });
